0dab87ceaf61ee3b5912bdf1552b16713d70640a
Vivien <vivienbenoy@gmail.com>
2021-03-21 18:37:02 +0000

    Regular Expressions
    Added regular expressions for validating caoNumber, date of birth and
    password

--- src/main/java/com/dkit/oopca5/client/RegexChecker.java
+++ src/main/java/com/dkit/oopca5/client/RegexChecker.java
@@ -3,6 +3,39 @@ package com.dkit.oopca5.client;
 /* This class should contain static methods to verify input in the application
  */
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexChecker
 {
+    public boolean verifyCAONumber(int caoNumber)
+    {
+        String regEx = "^[0-9]{8}$";
+        Pattern patternCAONumber= Pattern.compile(regEx);
+        Matcher matcherCAONumber=patternCAONumber.matcher(String.valueOf(caoNumber));
+        if(matcherCAONumber.matches())
+        {
+            return true;
+        }
+        return false;
+    }
+    public boolean verifyDateOfBirth(String dateOfBirth){
+        String regExDateOfBirth="^\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])$";
+        Pattern patternDateOfBirth=Pattern.compile(regExDateOfBirth);
+        Matcher matcherDateOfBirth= patternDateOfBirth.matcher(dateOfBirth);
+        if(matcherDateOfBirth.matches())
+        {
+            return true;
+        }
+        return false;
+    }
+    public boolean verifyPasswrd(String password){
+        String regExPassword=".{8,}";
+        Pattern patternPassword=Pattern.compile(regExPassword);
+        Matcher matcherPassword=patternPassword.matcher(password);
+        if(matcherPassword.matches()){
+            return true;
+        }
+        return false;
+    }
 }
